4e4badf7ac22b62a793f3f64d8b021ab15ecbb0c
augustoorsi <augusford@hotmail.com>
2023-06-12 19:30:21 -0300

    propertyRoutes eliminado

--- Server/src/routes/index.ts
+++ Server/src/routes/index.ts
@@ -1,10 +1,9 @@
 import { Router } from "express";
-import propertyRoutes from "./propertyRoutes";
 import usersRouter from "./usersRouter/usersRouter";
 
 const router: Router = Router();
 
 router.use('/users', usersRouter)
-router.use('/property', propertyRoutes);
+
 
 export default router;
