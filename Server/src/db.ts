c5761a1118fa30e197244baf114c6a2d39808aba
franciscogalliani <franemi2k22@gmail.com>
2023-06-12 12:59:26 -0300

    database created

---
+++ Server/src/db.ts
@@ -0,0 +1,12 @@
+import dotenv from 'dotenv';
+import { Sequelize } from 'sequelize';
+
+dotenv.config();
+const { DB_USER, DB_PASSWORD, DB_HOST } = process.env;
+
+const sequelize: Sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/arbnb`, {
+    logging: false,
+    native: false
+});
+
+export default sequelize;
