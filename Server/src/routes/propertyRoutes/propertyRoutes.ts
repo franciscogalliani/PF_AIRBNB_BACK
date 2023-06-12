60c350f285514d295b180caf810e08a7a3e61723
unknown <alelopez.13.97@gmail.com>
2023-06-12 17:04:34 -0300

    propHandlers

--- Server/src/routes/propertyRoutes/propertyRoutes.ts
+++ Server/src/routes/propertyRoutes/propertyRoutes.ts
@@ -1,10 +1,12 @@
-const { Router } = require('express');
-const { getAllPropHandler, getPropById, createPropHandler } = require('../handlers/propertyHandlers')
+import { Router } from 'express';
+import { getAllPropHandler, getPropById, createPropHandler } from '../../handlers/propertyHandlers';
 
 const propertyRoutes = Router();
 
 propertyRoutes.get('/', getAllPropHandler);
+
 propertyRoutes.get('/:id', getPropById);
+
 propertyRoutes.post('/', createPropHandler);
 
 export default propertyRoutes;
