df0747c6973ea643dc42066b2d98be35cf77fedf
augustoorsi <augusford@hotmail.com>
2023-06-13 14:46:05 -0300

    put andando

--- Server/src/routes/propertyRoutes/controllers/createPropController.ts
+++ Server/src/routes/propertyRoutes/controllers/createPropController.ts
@@ -1,11 +1,12 @@
 import sequelize from "../../../db"
+import { PropertyAttributes } from "../../../models/Interfaces";
 
 const {Properties} = sequelize.models
 
 
-const createPropController = async (newProperty) => {
-    const createProperty = await Properties.create(newProperty)
-
+const createPropController = async (newProperty:Partial<PropertyAttributes>) => {
+    const createdProperty = await Properties.create(newProperty)
+    return  createdProperty
 }
 
 export default createPropController;
--- Server/src/routes/propertyRoutes/handlers/createPropHandler.ts
+++ Server/src/routes/propertyRoutes/handlers/createPropHandler.ts
@@ -4,56 +4,9 @@ import { PropertyAttributes } from "../../../models/Interfaces";
 
 
 const createPropHandler = async(req: Request, res: Response) => {
-    // const {
-    //     title,
-    //     province,
-    //     location,
-    //     address,
-    //     zip_code,
-    //     property_type,
-    //     description,
-    //     price_per_night,
-    //     images,
-    //     rating,
-    //     ratings_amount,
-    //     start_date,
-    //     end_date,
-    //     rooms_number,
-    //     bathrooms_number,
-    //     beds_number,
-    //     max_guests,
-    //     allow_pets,
-    //     weekly_discount,
-    //     monthly_discount,
-    //     min_nights
-    // } = req.body;
-
-    const newProperty = {
-        title,
-        province,
-        location,
-        address,
-        zip_code,
-        property_type,
-        description,
-        price_per_night,
-        images,
-        rating,
-        ratings_amount,
-        start_date,
-        end_date,
-        rooms_number,
-        bathrooms_number,
-        beds_number,
-        max_guests,
-        allow_pets,
-        weekly_discount,
-        monthly_discount,
-        min_nights
-    }
-
+    const newProperty = req.body as PropertyAttributes
     try {
-        const response = await createPropController(req.body)
+        const response = await createPropController(newProperty)
         return res.status(200).json(response)
     } catch (error: any) {
         return res.status(400).json({Error: error.message})
