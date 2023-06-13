ca2027ac7cc5943453eef54f4c56070390c6cc49
Benny Wiliams <105727859+bennywr20@users.noreply.github.com>
2023-06-13 21:45:35 +0200

    cloudinary

---
+++ Server/src/utils/cloudinary.ts
@@ -0,0 +1,32 @@
+import Cloudinary from "cloudinary";
+import dotenv from 'dotenv';
+
+
+dotenv.config()
+const { CLOUDINARY_CLOUD_NAME, CLOUDINARY_API_KEY, CLOUDINARY_SECRET } = process.env;
+
+
+
+
+
+const cloudinary = Cloudinary.v2
+cloudinary.config({
+    cloud_name: CLOUDINARY_CLOUD_NAME,
+    api_key: CLOUDINARY_API_KEY,
+    api_secret: CLOUDINARY_SECRET,
+    secure: true
+});
+
+
+
+export const imageUpdate = async (filePath: any) => {
+    return await cloudinary.uploader.upload(filePath, {
+        folder: 'Argentina'
+    })
+}
+
+export const imageDestroy = async (id: string) => {
+    return await cloudinary.uploader.destroy(id)
+}
+
+
