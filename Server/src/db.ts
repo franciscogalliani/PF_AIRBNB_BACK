b92c08da040247e0d2b471f839c373669618c2f5
franciscogalliani <franemi2k22@gmail.com>
2023-06-13 21:08:54 -0300

    fix

--- Server/src/db.ts
+++ Server/src/db.ts
@@ -11,7 +11,7 @@ const { DB_USER, DB_PASSWORD, DB_HOST } = process.env;
 
 // const sequelize: Sequelize = new Sequelize(`${DB_USER}://postgres:${DB_PASSWORD}@${DB_HOST}:7823/railway`
 
-const sequelize: Sequelize = new Sequelize(`${DB_USER}://postgres:${DB_PASSWORD}@${DB_HOST}:7823/railway`, {
+const sequelize: Sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/arbnb`, {
     logging: false,
     native: false
 });
