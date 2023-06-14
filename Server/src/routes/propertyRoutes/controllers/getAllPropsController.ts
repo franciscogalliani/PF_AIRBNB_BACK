b141aa46348d7744307e2ce5a3cc5482a8d2f46d
augustoorsi <augusford@hotmail.com>
2023-06-14 17:58:05 -0300

    cambios realizados

--- Server/src/routes/propertyRoutes/controllers/getAllPropsController.ts
+++ Server/src/routes/propertyRoutes/controllers/getAllPropsController.ts
@@ -1,26 +1,8 @@
-import { Op } from 'sequelize'
 import  sequalize  from '../../../db'
 
 const { Properties, Ratings, Services } = sequalize.models
 
-const getAllPropsController = async (name: string | void) => {
-    if(name){
-        const response = await Properties.findAll({
-            include: {
-                model: Services,
-                attributes: ['name', 'icon'],
-                through: {
-                    attributes: []
-                }
-            },
-            where: {
-                title: { [Op.iLike]: `%${name}%`}
-            }
-        })
-        if(response.length > 0){
-            return response
-        } return 'No hay propiedades con ese nombre'
-    }
+const getAllPropsController = async () => {
     const response = await Properties.findAll({
         include: {
             model: Services,
