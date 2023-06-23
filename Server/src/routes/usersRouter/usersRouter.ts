09cc56622ddcfd900bbc6fde12248535452cbe08
augustoorsi <augusford@hotmail.com>
2023-06-23 12:22:27 -0300

    Favoritos realizados

--- Server/src/routes/usersRouter/usersRouter.ts
+++ Server/src/routes/usersRouter/usersRouter.ts
@@ -3,6 +3,9 @@ import createUsersHandler from "./handler/createUsersHandler";
 import getUsersHandler from "./handler/getUsersHandler";
 import getUsersByIdHandler from "./handler/getUsersByIdHandler";
 import updateUsersHandler from "./handler/updateUsersHandler";
+import updateFavoritesHandler from "./handler/updateFavoritesHandler"
+import getFavoritesHandler from "./handler/getFavoritesHandler";
+import deleteFavoriteHandler from "./handler/deleteFavoriteHandler";
 
 const usersRouter: Router = Router();
 
@@ -10,9 +13,16 @@ usersRouter.get("/", getUsersHandler)
 
 usersRouter.get("/:id", getUsersByIdHandler)
 
+usersRouter.get("/:id_user/favorites", getFavoritesHandler)
+
 usersRouter.post('/', createUsersHandler)
 
+usersRouter.post('/:id_user/favorites', updateFavoritesHandler)
+
+usersRouter.delete('/:id_user/favorites/:id_property', deleteFavoriteHandler)
+
 usersRouter.put('/update/:id', updateUsersHandler)
+
 
 
 
