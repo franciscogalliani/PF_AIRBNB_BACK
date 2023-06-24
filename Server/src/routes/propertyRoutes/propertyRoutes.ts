54e5d9612804cb2fc7e06cb6caa306a0d9b774f1
MercedesPoisson <mercedespoisson@yahoo.com>
2023-06-24 20:00:07 -0300

    Rutas get All sin filtros

--- Server/src/routes/propertyRoutes/propertyRoutes.ts
+++ Server/src/routes/propertyRoutes/propertyRoutes.ts
@@ -4,10 +4,13 @@ import getPropByIdHandler from './handlers/getPropByIdHandler';
 import createPropHandler from './handlers/createPropHandler';
 import updatePropertyStatusHandler from './handlers/updatePropertyStatusHandler';
 import updatePropertyHandler from './handlers/updatePropertyHandler';
+import getAllPropertiesDashHandler from './handlers/getAllPropertiesDashHandler';
 
 const propertyRoutes = Router();
 
 propertyRoutes.get('/', getAllPropHandler);
+
+propertyRoutes.get('/all', getAllPropertiesDashHandler);
 
 propertyRoutes.get('/:id', getPropByIdHandler);
 
