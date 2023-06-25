430a342de0ce17a3c78ca87a19b740cc84ef2976
franciscogalliani <franemi2k22@gmail.com>
2023-06-25 13:53:34 -0300

    Mercadopago

--- Server/src/app.ts
+++ Server/src/app.ts
@@ -5,6 +5,7 @@ import router from './routes';
 import cors from 'cors';
 import multer from 'multer';
 import sequelize from './db';
+import mercadopago from 'mercadopago'
 
 const { Rents, Users, Properties } = sequelize.models
 
@@ -13,6 +14,8 @@ interface CustomError extends Error {
 }
 
 const server: Express = express();
+
+mercadopago.configurations.setAccessToken("TEST-7287226739695489-062410-edcf3670bf07cd7e101763f59926a773-761450099");
 
 server.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
 server.use(bodyParser.json({ limit: '50mb' }));
@@ -47,5 +50,26 @@ server.use((err: CustomError, req: Request, res: Response, next: NextFunction): 
     console.error(err);
     res.status(status).send(message)
 });
+
+server.post('/process_payment', (req, res) => {
+    let payment_data = {
+      token: req.body.formData.token,
+      issuer_id: req.body.formData.issuer_id,
+      payment_method_id: req.body.formData.payment_method_id,
+      transaction_amount: req.body.formData.transaction_amount,
+      installments: req.body.formData.installments,
+      payer: req.body.formData.payer
+    }
+    console.log(payment_data)
+    mercadopago.payment.save(payment_data)
+      .then(function(response) {
+        const { status, status_detail, id } = response.body;
+        console.log(response)
+        res.status(response.status).json({ status, status_detail, id });
+      })
+      .catch(function(error) {
+        console.error(error);
+      });
+  })
 
 export default server;
