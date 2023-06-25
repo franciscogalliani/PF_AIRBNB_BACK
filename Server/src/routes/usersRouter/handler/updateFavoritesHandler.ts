be4f8a6a0fa477e6575390414a129e97dcbdcad4
augustoorsi <augusford@hotmail.com>
2023-06-25 20:00:38 -0300

    cambios hechos

--- Server/src/routes/usersRouter/handler/updateFavoritesHandler.ts
+++ Server/src/routes/usersRouter/handler/updateFavoritesHandler.ts
@@ -2,8 +2,8 @@ import { Request, Response } from "express";
 import updateFavorites from "../controller/updateFavoritesController";
 
 const updateFavoritesHandler = async(req: Request, res: Response) => {
-    const { id_user } = req.params
-    const {id_property} = req.body
+    const { id_user, id_property } = req.params
+    
     try {
         const result = await updateFavorites(id_user, id_property)
         res.status(200).json(result)
