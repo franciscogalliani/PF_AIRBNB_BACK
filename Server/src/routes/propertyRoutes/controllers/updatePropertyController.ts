0f4390311d292deeca6a90d8ec1638a166f6e2a0
augustoorsi <augusford@hotmail.com>
2023-06-27 15:58:38 -0300

    "update"

--- Server/src/routes/propertyRoutes/controllers/updatePropertyController.ts
+++ Server/src/routes/propertyRoutes/controllers/updatePropertyController.ts
@@ -1,14 +1,23 @@
 import sequalize from '../../../db';
 
-const { Properties } = sequalize.models;
+const { Properties, Services } = sequalize.models;
 
 const updateProperty = async (id: number, updatedValues: Partial<typeof Properties>) => {
+  const {services} = updatedValues
+  console.log(services);
+  
   try {
-    await Properties.update(updatedValues, {
+      await Properties.update(updatedValues, {
       where: {
         id_property: id,
       },
     });
+    if(services){
+      const property = await Properties.findByPk(id)
+      await property.setServices([])
+      const servicesDB = await Services.findAll({where:{name:services}})
+      await property.addServices(servicesDB)
+    }
   } catch (error) {
     if (error instanceof Error) {
       throw new Error(error.message);
