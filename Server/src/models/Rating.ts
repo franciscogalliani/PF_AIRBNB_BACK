56b80da6f041527f427fe4c393977f094ecb6052
MercedesPoisson <mercedespoisson@yahoo.com>
2023-06-30 14:41:28 -0300

    nueva ruta put ratings

--- Server/src/models/Rating.ts
+++ Server/src/models/Rating.ts
@@ -56,7 +56,24 @@ const Rating = (sequelize: Sequelize) => {
         date_rating: {
             type: DataTypes.DATEONLY,
             allowNull: false
+        },
+        is_active: {
+            type: DataTypes.BOOLEAN,
+            allowNull: true,
+            defaultValue: true
+        },
+        is_reported: {
+            type: DataTypes.BOOLEAN,
+            allowNull: true,
+            defaultValue: false
+        },
+        report_reason: {
+            type: DataTypes.STRING(200),
+            allowNull: true,
+            defaultValue: false
+
         }
+
     },
     {
         timestamps: false
